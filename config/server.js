23e646930b2ad28879bdb8005439d19f30e37b9f
Duviel Garcia <duviel7@gmail.com>
2021-03-17 21:23:09 -0500

    First commit, creación de modelos propiedades, categorias, agentes y las paginas del sitio

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '218f084c91589d1521afc06863f4ec77'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '35209612-67b7-45b5-8d8d-60e16f0f1d08'
+};
